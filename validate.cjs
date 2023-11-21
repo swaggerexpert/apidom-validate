abdee62f64b28de96582ac971d54d06c55c1e590
amandalian <amandalianhj@gmail.com>
2023-11-22 02:32:35 +0800

    correct failsOn value datatype

--- validate.cjs
+++ validate.cjs
@@ -63,16 +63,16 @@ const mapDiagnostics = (diagnostics) => {
 
   let condition;
   switch (failsOn) {
-    case 1:
+    case '1':
       condition = errors.length > 0;
       break;
-    case 2:
+    case '2':
       condition = warnings.length > 0;
       break;
-    case 3:
+    case '3':
       condition = information.length > 0;
       break;
-    case 4:
+    case '4':
       condition = hints.length > 0;
       break;
     default:
